63b90069272d866502c49ad87c0f5f6ee9698e3e
Sheld5 <simonsoldat@seznam.cz>
2021-03-12 18:35:36 +0100

    create test view

--- src/main/java/soldasim/MTG_ER_Table/View/View.java
+++ src/main/java/soldasim/MTG_ER_Table/View/View.java
@@ -1,8 +1,15 @@
 package soldasim.MTG_ER_Table.View;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import soldasim.MTG_ER_Table.Controller.Controller;
 
@@ -13,11 +20,14 @@ import soldasim.MTG_ER_Table.Controller.Controller;
 public class View extends Application implements Runnable {
 
     private static final String WINDOW_TITLE = "MTG ER Table";
+    private static final Insets PADDING = new Insets(12, 12, 12, 12);
+    private static final int SPACING = 12;
 
     private Controller controller;
 
     private Stage mainStage;
     private Scene testScene;
+    private ImageView imageView;
 
     /**
      * Save a reference to the application controller.
@@ -54,9 +64,33 @@ public class View extends Application implements Runnable {
         mainStage.setScene(scene);
     }
 
+    // TODO
     private void initTestScene() {
-        testScene = new Scene(new Pane());
-        //TODO
+                    imageView = new ImageView();
+
+                Pane imagePane = new Pane(imageView);
+                imagePane.setPrefSize(256, 256);
+
+                    TextArea deckList = new TextArea();
+
+                    Button loadButton = new Button("load");
+                    loadButton.setOnAction(event -> loadButtonPressed());
+
+                VBox deckArea = new VBox(deckList, loadButton);
+                deckArea.setAlignment(Pos.CENTER);
+                deckArea.setSpacing(SPACING);
+                deckArea.setPrefSize(128, 256);
+
+            HBox content = new HBox(imagePane, deckArea);
+            content.setAlignment(Pos.CENTER);
+            content.setSpacing(SPACING);
+            content.setPadding(PADDING);
+
+        testScene = new Scene(content);
+    }
+
+    private void loadButtonPressed() {
+        
     }
 
 }
