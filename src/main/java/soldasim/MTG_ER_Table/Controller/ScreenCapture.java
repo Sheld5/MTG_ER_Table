aa69e08c1666bf75d3a9471233d0dcc1ae72398f
Sheld5 <simonsoldat@seznam.cz>
2021-03-14 19:10:59 +0100

    implement screenshotting window by its handle

--- src/main/java/soldasim/MTG_ER_Table/Controller/ScreenCapture.java
+++ src/main/java/soldasim/MTG_ER_Table/Controller/ScreenCapture.java
@@ -7,6 +7,8 @@ import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
 import soldasim.MTG_ER_Table.View.View;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.List;
 
 /**
@@ -17,8 +19,11 @@ public class ScreenCapture {
     private static final int MAX_TITLE_LENGTH = 512;
     private static final int FW_TITLE_REFRESH_RATE = 60; // times per second
 
-    private static ForegroundWindowUpdater fwUpdater;
     private static View view;
+    private static ForegroundWindowUpdater fwUpdater;
+    private static String fwTitle = "";
+    private static WinDef.HWND fwHandle;
+    private static Robot robot;
 
     /**
      * Return a list of all active windows.
@@ -29,15 +34,43 @@ public class ScreenCapture {
     }
 
     /**
-     * Return the title of the window in the foreground.
-     * @return a String containing the title
+     * Return the WinDef.HWND handle of the window in the foreground.
+     * @return WinDef.HWND of the window in the foreground
      */
-    public static String getForegroundWindowTitle() {
+    public static WinDef.HWND getForegroundWindow() {
+        return User32.INSTANCE.GetForegroundWindow();
+    }
+
+    /**
+     * Return the title of the given window.
+     * @param window WinDef.HWND handle of a window
+     * @return the title of the given window
+     */
+    public static String getWindowTitle(WinDef.HWND window) {
         char[] title = new char[MAX_TITLE_LENGTH];
-        User32 user = User32.INSTANCE;
-        WinDef.HWND foregroundWindow = user.GetForegroundWindow();
-        user.GetWindowText(foregroundWindow, title, MAX_TITLE_LENGTH);
+        User32.INSTANCE.GetWindowText(window, title, MAX_TITLE_LENGTH);
         return Native.toString(title);
+    }
+
+    /**
+     * Return a screenshot of the given window.
+     * @param window WinDef.HWND handle of the window
+     * @return BufferedImage containing an image of the window
+     *         Can return null on error or if the given window does not have positive dimensions.
+     */
+    public static BufferedImage captureWindow(WinDef.HWND window) {
+        if (robot == null) {
+            try {
+                robot = new Robot();
+            } catch (AWTException e) {
+                return null;
+            }
+        }
+        WinDef.RECT RECT = new WinDef.RECT();
+        User32.INSTANCE.GetWindowRect(window, RECT);
+        Rectangle border = RECT.toRectangle();
+        if (!(border.width > 0 && border.height > 0)) return null;
+        return robot.createScreenCapture(RECT.toRectangle());
     }
 
     /**
@@ -79,7 +112,6 @@ public class ScreenCapture {
     private static class ForegroundWindowUpdater implements Runnable {
 
         private boolean run = true;
-        private String windowTitle = "";
 
         @Override
         public void run() {
@@ -88,10 +120,12 @@ public class ScreenCapture {
             while (run) {
                 long startTime = System.currentTimeMillis();
 
-                String newWindowTitle = getForegroundWindowTitle();
-                if (!(newWindowTitle.equals(windowTitle) || newWindowTitle.equals(thisWindowTitle))) {
-                    windowTitle = newWindowTitle;
-                    view.giveForegroundWindowTitle(windowTitle);
+                WinDef.HWND newWindow = getForegroundWindow();
+                String newWindowTitle = getWindowTitle(newWindow);
+                if (!(newWindowTitle.equals(fwTitle) || newWindowTitle.equals(thisWindowTitle))) {
+                    fwHandle = newWindow;
+                    fwTitle = newWindowTitle;
+                    view.giveForegroundWindowTitle(fwTitle);
                 }
 
                 long waitTime = (long)(1000 / FW_TITLE_REFRESH_RATE) - (System.currentTimeMillis() - startTime);
@@ -103,6 +137,24 @@ public class ScreenCapture {
             }
         }
 
+    }
+
+    /**
+     * Return the title of the last application window that has been in the foreground
+     * while the ForegroundWindowUpdater was running excluding the window of this application.
+     * @return String containing title of the last foreground application window
+     */
+    public static String getLastFWTitle() {
+        return fwTitle;
+    }
+
+    /**
+     * Return the WinDef.HWND handle of the last application window that has been in the foreground
+     * while the ForegroundWindowUpdater was running excluding the window of this application.
+     * @return WinDef.HWND of the last foreground application window
+     */
+    public static WinDef.HWND getLastFWHandle() {
+        return fwHandle;
     }
 
 }
